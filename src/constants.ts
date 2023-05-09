2805641739fe91e0968043ec06de9357dfbc46a4
Mark Trevino <marktrevino@microsoft.com>
2023-05-09 16:03:59 -0700

    basic tab groups in explorer tab working
    Co-authored-by: hanjenny-ms <hanjenny-ms@users.noreply.github.com>

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+
+
+export const CommandNames = {
+    HelloWorld: 'tab-groups.helloWorld',
+    Save: 'tab-groups.save',
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,31 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { commands, window, ExtensionContext, workspace } from 'vscode';
+import { CommandNames } from './constants';
+import { GroupProvider } from './groupProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "tab-groups" is now active!');
+	const rootPath = (workspace.workspaceFolders && (workspace.workspaceFolders.length > 0))
+		? workspace.workspaceFolders[0].uri.fsPath : undefined;
+	const groupProviders = new GroupProvider(rootPath);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tab-groups.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tab-groups!');
-	});
+	window.registerTreeDataProvider('tab-groups', groupProviders);
+	
+	let disposable = [
+		commands.registerCommand(CommandNames.HelloWorld, () => {
+		window.showInformationMessage('Hello World from tab-groups!');
+	}),
+		commands.registerCommand(CommandNames.Save, () => {
+	}),
+];
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.concat(disposable);
 }
 
 // This method is called when your extension is deactivated
