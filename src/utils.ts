fd43549e3fad906f976c16485ff042ef779dc05c
Mark Trevino <marktrevino@microsoft.com>
2023-05-11 13:30:10 -0700

    add addtogroup method (still needs functionality)

--- src/utils.ts
+++ src/utils.ts
@@ -10,26 +10,41 @@ export async function saveGroup(groupProvider: GroupProvider): Promise<boolean> 
 
     if (name === undefined) { return false; }
     
-    console.log('window.tabGroups.all.length: ' + window.tabGroups.all.length);
+    // console.log('window.tabGroups.all.length: ' + window.tabGroups.all.length);
     window.tabGroups.all.map(group => {
         groupProvider.add(name ?? '', group);
-        console.log('1')
-        // TODO: PREVENT GROUPS FROM BEING OVERWRITTEN
+        // console.log('1');
+        // TODO: PREVENT GROUPS FROM BEING OVERWRITTEN WHEN SPLIT VIEW IS USED
     });
 
     return true;
 }
 
 export async function addToGroup(groupProvider: GroupProvider): Promise<boolean> {
-    groupProvider.groups;
-    let name = await window.showInputBox({
-        placeHolder: 'Please enter a name for the group you would like to add'    
+    let tabToAdd = await window.showQuickPick(getAllTabsFromTabGroups(), {
+        placeHolder: 'Please select the file you would like to add to a group',
     });
 
-    if (name === undefined) { return false; }
-    console.log('window.tabGroups.all.length: ' + window.tabGroups.all.length);
-    window.tabGroups.all.map(group => groupProvider.add(name ?? '', group));
+    console.log(tabToAdd);
+    let groupNames = [];
+    for (let key in groupProvider.groups) {
+        groupNames.push(key);
+    }
 
+    if(groupNames.length === 0) {
+        let name = await window.showInputBox({
+            placeHolder: 'You dont have any groups yet, please enter a name for a new group'
+        });
+        if (name === undefined) { return false; }
+        console.log('name: ' + name);
+        console.log('window.tabGroups.all.length: ' + window.tabGroups.all.length);
+        // window.tabGroups.all.map(group => groupProvider.add(name ?? '', group));
+    }
+    let groupToAddTo = await window.showQuickPick(groupNames, {
+        placeHolder: 'Please select the group you would like to add the file to'
+    });
+
+    
     return true;
 }
 
@@ -62,4 +77,16 @@ export async function openFile(item: FileTreeItem): Promise<void> {
     console.log(item.getData() as Tab);
     return;
 
+}
+
+function getAllTabsFromTabGroups() {
+    let tabgroups = window.tabGroups.all;
+    let groupTabs = tabgroups.map(tabGroup => tabGroup.tabs);
+    return groupTabs.map(tabArray => tabArray.map(tab => tab.label)).flat();
+}
+
+function getTabFromTabGroups(tabString: string) {
+    // let tabGroupsArray = window.tabGroups.all;
+    // let tabGroups = tabGroupsArray.map(tabGroup => tabGroup.tabs);
+    // return tabGroups.map(tabArray => tabArray.find(tabString)).flat();
 }
